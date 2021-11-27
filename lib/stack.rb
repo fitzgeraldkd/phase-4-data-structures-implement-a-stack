62f527bc28ef199110a5b1b35172e5ff8be178f6
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-11-27 14:44:34 -0500

    commit tree 406e285bc80024cabf69119ccd3b5937c84c4552

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,43 @@
 # your code here
+class Stack
+  def initialize(limit=0)
+    @stack = []
+    @limit = limit
+  end
+
+  def push(value)
+    raise "Stack Overflow" if size >= @limit && @limit != 0
+    @stack.push(value)
+  end
+
+  def pop
+    value = peek
+    @stack.pop
+    value
+  end
+
+  def peek
+    @stack.last
+  end
+
+  def size
+    @stack.count
+  end
+
+  def empty?
+    size == 0
+  end
+
+  def full?
+    size == @limit
+  end
+
+  def search(value)
+    i = size - 1
+    until i < 0
+      return size - i - 1 if @stack[i] == value
+      i -= 1
+    end
+    -1
+  end
+end
